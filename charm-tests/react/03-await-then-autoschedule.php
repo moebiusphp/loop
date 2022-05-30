f2e6383d0e6671a1c25fd14e71a16bdb7d34c407
Frode Børli <frode@ennerd.com>
2022-05-30 20:43:53 +0000

    A test was not run for other drivers than react.

--- charm-tests/react/03-await-then-autoschedule.php
+++ charm-tests/react/03-await-then-autoschedule.php
@@ -1,11 +1,6 @@
 <?php
 require(__DIR__.'/../../vendor/autoload.php');
 
-if (!class_exists(React\EventLoop\Loop::class)) {
-    echo "--SKIP--\n";
-    return;
-}
-
 $p = new Moebius\Promise(function($yes) {
     $yes(true);
 });
