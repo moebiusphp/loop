1c2418b876e290fccb194afbb333e51e6a31fadc
Frode Børli <frode@ennerd.com>
2022-05-31 07:01:35 +0000

    Working with React driver

--- src/Drivers/ReactDriver.php
+++ src/Drivers/ReactDriver.php
@@ -6,14 +6,19 @@ use Moebius\Loop\{
     DriverInterface
 };
 use React\EventLoop\Loop;
+use React\EventLoop\LoopInterface;
+
 
 class ReactDriver implements DriverInterface {
 
+    private LoopInterface $loop;
     private Closure $exceptionHandler;
+    private bool $shutdownTriggered = false;
     private bool $wasStopped = false;
 
     public function __construct(Closure $exceptionHandler) {
         $this->exceptionHandler = $exceptionHandler;
+        $this->loop = \React\EventLoop\Factory::create();
         \register_shutdown_function($this->onShutdown(...));
     }
 
@@ -56,16 +61,21 @@ class ReactDriver implements DriverInterface {
     public function run(): void {
         Loop::run();
         // when running the event loop, React will set its internal running state to true
-        $this->wasStopped = true;
+        if (!$this->shutdownTriggered) {
+            \register_shutdown_function($this->run(...));
+        }
     }
 
     public function stop(): void {
         Loop::stop();
         // if we are forced to stop the event loop, React will set its internal running state to false
-        $this->wasStopped = true;
+        if (!$this->shutdownTriggered) {
+            \register_shutdown_function($this->run(...));
+        }
     }
 
     private function onShutdown(): void {
+        $this->shutdownTriggered = true;
         if ($this->wasStopped) {
             \register_shutdown_function(\React\EventLoop\Loop::run(...));
         }
